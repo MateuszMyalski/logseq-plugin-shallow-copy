e2c9ec9c0e4807189f1a2012f8da87712e2931e0
Mateusz Myalski <mateusz.myalski@gmail.com>
2023-12-03 14:32:44 +0100

    Added copy to clipboard option

--- index.js
+++ index.js
@@ -6,12 +6,21 @@
  * @author Mateusz Myalski
  */
 
+const pluginSettings = [{
+  key: "copyToClipboard",
+  default: false,
+  description: "Copy the block's children into the clipboard instead current journal page.",
+  title: "Copy block's children into the clipboard",
+  type: "boolean",
+}];
+
 function main() {
   logseq.Editor.registerBlockContextMenuItem('Shallow copy', (e) => shallowCopyBlocks(e))
 }
+
 async function shallowCopyBlocks(event) {
   try {
-    const blockUUID = event.uuid
+    const blockUUID = event.uuid;
     const parentBlockEntity = await logseq.Editor.getBlock(blockUUID);
 
     if (parentBlockEntity.children.length === 0) {
@@ -19,17 +28,54 @@ async function shallowCopyBlocks(event) {
       return;
     }
 
-    const { parentName, children } = await createBlocksBatch(parentBlockEntity);
+    if (logseq.settings.copyToClipboard) {
+      await copyToClipboard(parentBlockEntity);
+    } else {
+      await copyToJournalPage(parentBlockEntity);
+    }
 
-    const todayJournalDate = await getTodayJournalDate();
-    const journalPageEntity = await createJournalPage(todayJournalDate);
-
-    await insertBlocksIntoPage(journalPageEntity.uuid, { content: parentName, children });
-
-    showCopyToTodayJournalSuccessMessage(parentBlockEntity.children.length, todayJournalDate);
   } catch (error) {
     handleCopyError(error);
   }
+}
+
+async function copyToClipboard(blockToCopyEntity) {
+  const { stringifiedBlocks, numberOfBlocks } = await createBlocksString(blockToCopyEntity);
+
+  window.focus()
+  await navigator.clipboard.writeText(stringifiedBlocks);
+
+  showCopyToClipboardSuccessMessage(numberOfBlocks);
+}
+
+async function copyToJournalPage(blockToCopyEntity) {
+  const { parentName, children } = await createBlocksBatch(blockToCopyEntity);
+
+  const todayJournalDate = await getTodayJournalDate();
+  const journalPageEntity = await createJournalPage(todayJournalDate);
+
+  await insertBlocksIntoPage(journalPageEntity.uuid, { content: parentName, children });
+
+  showCopyToTodayJournalSuccessMessage(blockToCopyEntity.children.length, todayJournalDate);
+
+}
+
+async function createBlocksString(parentBlockEntity) {
+  let stringifiedBlocks = "";
+  let numberOfBlocks = 0;
+
+  const parentName = parentBlockEntity.content;
+  stringifiedBlocks += `- ${parentName}\n`;
+
+  const children = await Promise.all(parentBlockEntity.children.map(async (element) => {
+    const elementBlockEntity = await logseq.Editor.getBlock(element[1]);
+    numberOfBlocks += 1;
+    return elementBlockEntity.content;
+  }));
+
+  children.forEach((el) => { stringifiedBlocks += `\t- ${el}\n`; });
+
+  return { stringifiedBlocks, numberOfBlocks };
 }
 
 async function createBlocksBatch(parentBlockEntity) {
@@ -64,10 +110,14 @@ function showCopyToTodayJournalSuccessMessage(blocksCount, todayJournalDate) {
   logseq.UI.showMsg(`Copied [${blocksCount}] blocks to ${todayJournalDate}!`);
 }
 
+function showCopyToClipboardSuccessMessage(blocksCount) {
+  logseq.UI.showMsg(`Copied [${blocksCount}] blocks to the clipboard!`);
+}
+
 function handleCopyError(error) {
   console.error('Error copying blocks to journal:', error);
   logseq.UI.showMsg('An error occurred while copying blocks to journal. Check the console for details.');
 }
 
 // bootstrap
-logseq.ready(main).catch(console.error);
+logseq.useSettingsSchema(pluginSettings).ready(main).catch(console.error);
